bf502bc96530b68e5f18109148c44f595769d3f2
Gorkem Gokcek <140439905+gorkemgokcek@users.noreply.github.com>
2023-11-19 18:27:43 +0300

    FSWeb-S5-Challenge/cevaplar

---
+++ tablar.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const Tablar = (konular) => {
+  // GÖREV 3
+  // ---------------------
+  // Tek argümanı bir dizi ("konular") olan bu fonksiyonu uygulayın.
+  // Örnek olarak, konular dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
+  // fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
+  // Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
+  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+  //
+  // <div class="topics">
+  //   <div class="tab">javascript</div>
+  //   <div class="tab">bootstrap</div>
+  //   <div class="tab">teknoloji</div>
+  // </div>
+  //
+  const divTopics = document.createElement("div");
+  divTopics.className = "topics";
+
+  konu.forEach(element => {
+    const divTab = document.createElement("div");
+    divTab.className = "tab";
+    divTab.textContent = element;
+    divTopics.append(divTab);
+    
+  });
+  return divTopics;
+
+}
+
+const tabEkleyici = (secici) => {
+  // GÖREV 4
+  // ---------------------
+  // Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
+  // Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
+  // Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
+  // Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
+  //
+
+  axios.get(`http://localhost:5001/api/konular`)
+  .then((res) => {
+    return res.data["konular"]
+  }).then((data) => {
+    document.querySelector(secici).append(Tablar(data));
+  })
+};
+
+export { Tablar, tabEkleyici }
